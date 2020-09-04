951ada4553758ab172d474e6c6e0d264f15573f9
Laura Chelaru <laura.a.chelaru@gmail.com>
2020-09-04 16:44:37 +0200

    completo con commenti

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $("document").ready(function() {
 
-// Esporre 5 numeri casuali. Li faremo da 1 a 50.
+// Generiamo 5 numeri casuali. Li faremo da 1 a 50, non ripetibili
 var arrayNrCasuali = [];
 
 while(arrayNrCasuali.length < 5) {
@@ -10,7 +10,7 @@ while(arrayNrCasuali.length < 5) {
   }
 }
 
-// Stampiamo in HTML il testo per l'utente
+// Stampiamo in HTML i numeri
 $("#numeri-display p#numeri-output").text(
   arrayNrCasuali[0] + ", " +
   arrayNrCasuali[1] + ", " +
@@ -19,51 +19,73 @@ $("#numeri-display p#numeri-output").text(
   arrayNrCasuali[4] + "."
 );
 
-// Parte un timer di 30 secondi
+// Timer di 30 secondi
 var secondi = 5;
+
 var countdown30 = setInterval(function () {
   $("#countdown #secondi").text(secondi);
-  if(secondi == 1) {
+  if(secondi == 1) {  // Aggiungiamo una chicca.. arrivati al secondo 1, l'html si modifica in "secondo rimanente"
     $("#secondi-rimanenti").text(" secondo rimanente");
-  } else if(secondi == 0) {
+  } else if(secondi == 0) {      // L'intervallo finisce al secondo 0
     clearInterval(countdown30);
-    $("#numeri-display").hide();
+    $("#numeri-display").hide();  // Si chiude il box dei numeri.. se no che gioco è?
     $("#numeri-input").removeClass("display-none");  // Appare il box perché l'utente inserisca i numeri
   }
   secondi--;
 }, 1000);
 
-// Dopo che l'utente avrà inserito i numeri e premuto "Controlla", controlliamo il risultato
-
+// Settiamo le variabili dei numeri inseriti dall'utente e quella dei numeri "azzeccati"
 var numeriUtente = [];
+var arrayGiuste = [];
 
+// Qui scriveremo il nostro risultato
+var risultato = "";
+
+// Al click su "controlla" parte il controllo e viene stampato il risultato
 $("#controlla").click(
   function() {
     $("#numeri-input > .numero-inserito").each(
       function() {
-        numeriUtente.push($(this).val());
+        numeriUtente.push($(this).val());  // Leggiamo il valore degli input inseriti dall'utente
       }
     );
-    var arrayGiuste = [];
-    var arraySbagliate = [];
-    for(var i = 0; i < numeriUtente.length; i++) {
+    for(var i = 0; i < numeriUtente.length; i++) {   // Iteriamo nell'array dei numeri inseriti e verifichiamo quali sono azzeccati
       if(arrayNrCasuali.includes(parseInt(numeriUtente[i]))) {
-        arrayGiuste.push(numeriUtente[i]);
-      } else {
-        arraySbagliate.push(numeriUtente[i]);
+        arrayGiuste.push(numeriUtente[i]);   // I numeri giusti vengono inseriti nell'array arrayGiuste
       }
     }
-    console.log("Numeri utente: " + numeriUtente);
+    console.log("Numeri utente: " + numeriUtente);  // Un plus, in console stampiamo i 2 array
     console.log("Array giuste: " + arrayGiuste);
-    console.log("Array sbagliate: " + arraySbagliate);
+    $("#numeri-input > .numero-inserito").val("");  // Puliamo gli input perché siano pronti per il prossimo round!
+    stampaRisultato();   // Calcoliamo il risultato con lo switch (vedi sotto)
+    $("#output-risultato").removeClass("display-none");  // Appare il box con il risultato
+    $("#output-risultato p").text(risultato);
   }
 );
 
-// function checkNumbers(arrayNrCasuali, numeriUtente) {
-//
-//   return arrayGiuste, arraySbagliate;
-// }
-// Diciamo all'utente quanti numeri ha indovinato
+// Funzione di assegnazione del risultato
+function stampaRisultato() {
+  switch (arrayGiuste.length) {
+    case 5:
+      risultato = "5 giusti! Sei un fenomeno!";
+      break;
+    case 4:
+      risultato = "4 giusti! Quasi perfetto!";
+      break;
+    case 3:
+      risultato = "3 giusti! Sufficiente, ma.. perché non riprovi?";
+      break;
+    case 2:
+      risultato = "2.. Non benissimo.. riprova!";
+      break;
+    case 1:
+      risultato = "Solo 1.. Non ci siamo.. ma non mollare, riprova!";
+      break;
+    default:
+      risultato = "Ehm.. riprova, dai..";
+  }
+  return risultato;
+}
 
 }
 );
